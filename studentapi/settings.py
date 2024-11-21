77e54e7aa9d46c14ba0d9c2ff0c99c9dd650b1df
Nidha fathima <nidhafathimatv@gmail.com>
2024-11-21 12:58:45 +0530

    change urls

--- studentapi/settings.py
+++ studentapi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,14 +87,18 @@ WSGI_APPLICATION = 'studentapi.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'studentapi_db',
-        'USER':'postgres',
-        'PASSWORD':'nidha',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'studentapi_db',
+    #     'USER':'postgres',
+    #     'PASSWORD':'nidha',
+    #     'HOST':'localhost',
+    #     'PORT':'5432',
+    # }
+
+    'default':dj_database_url.config(default='postgresql://product_management_v6fn_user:SNR3gIIjXuGzQhVuIS6jehpOaj4c1TAI@dpg-csu44gd2ng1s73cc5ung-a.oregon-postgres.render.com/product_management_v6fn')
+
+    
 }
 
 
